77e89e632d6e5e54f8b397d5489e9a5157e55b5b
Natalia Lopez <natigab.24@gmail.com>
2024-02-06 15:57:21 -0300

    agrego Row en el componente Inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,7 +1,13 @@
+import { Row } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 
 const Inicio = () => {
-    return <Image src="/assets/banner.png" fluid />;
+    return (
+   <>
+    <Image src="/assets/banner.png" fluid />;
+    <Row></Row>
+    </>
+    )
 };
 
 export default Inicio;
