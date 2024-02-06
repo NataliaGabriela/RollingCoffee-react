8a6367236adef95f2974bb44d2c20d0bc00cdece
Natalia Lopez <natigab.24@gmail.com>
2024-02-06 14:01:40 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import NavBar from './components/NavBar';
+
+function App() {
+ 
+  return (
+    <>
+    <NavBar></NavBar>
+      <h1>hola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../App.css'
+
+const NavBar = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home"><img src="/assets/Coffee_Logo.png" className='ImgRollingCoffe'></img></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" >
+              <Nav className="ms-auto">
+                <Nav.Link href="#home" active>Inicio</Nav.Link>
+                <Nav.Link href="#link">Administrador</Nav.Link>
+                <Nav.Link href="#link">Registro</Nav.Link>
+                <Nav.Link href="#link">Login</Nav.Link>
+                
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+};
+
+export default NavBar;
