2cfa3227b4b48e32d514fc5774fbd86630d86468
Natalia Lopez <natigab.24@gmail.com>
2024-02-06 15:51:22 -0300

    coloqué banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import { Container } from 'react-bootstrap';
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import Inicio from "./components/Inicio";
 function App() {
   return (
     <>
-    <NavBar></NavBar>
-    <Container className='mainPage'></Container>
-    <Footer></Footer>
       <NavBar></NavBar>
+      <Container fluid className="mainPage">
+        <Inicio></Inicio>
+      </Container>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,7 @@
+import Image from 'react-bootstrap/Image';
+
+const Inicio = () => {
+    return <Image src="/assets/banner.png" fluid />;
+};
+
+export default Inicio;
