1420dd2c5867d8775f6faba3438dfc6b24c64160
Natalia Lopez <natigab.24@gmail.com>
2024-02-06 15:05:13 -0300

    terminé el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import NavBar from './components/NavBar';
-
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 function App() {
  
   return (
     <>
     <NavBar></NavBar>
-      
+    <Container className='mainPage'></Container>
+    <Footer></Footer>
     </>
   )
 }
