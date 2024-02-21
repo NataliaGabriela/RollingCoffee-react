f065ae6f4f2f9a559ad39695adabc4806146224e
Natalia Lopez <natigab.24@gmail.com>
2024-02-21 01:35:00 -0300

    configuramos el json server

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,11 +1,17 @@
-import { Image } from 'react-bootstrap';
-import imgError from '../../../assets/error404.png';
+import { Button, Image } from "react-bootstrap";
+import imgError from "../../../assets/error404.png";
+import { Link } from "react-router-dom";
 const Error = () => {
-    return (
-        <div className='tx-center d-flex justify-content-center'>
-            <Image src={imgError} className='imagenError' fluid/>
-        </div>
-    );
+  return (
+    <>
+      <div className="tx-center d-flex justify-content-center">
+        <Image src={imgError} className="imagenError" fluid />
+      </div>
+      <Button as={Link} to="/" variant="success" className="botonError">
+        Volver al Inicio
+      </Button>
+    </>
+  );
 };
 
 export default Error;
