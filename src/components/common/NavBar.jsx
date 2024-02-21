21d62e21e03b9a80339e5125ac6e458448d65933
Natalia Lopez <natigab.24@gmail.com>
2024-02-21 00:52:37 -0300

    agregamos las rutas correspondientes en las opciones del menu

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,20 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../../App.css'
+import '../../App.css';
+import {Link, NavLink} from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="#home"><img src="/assets/Coffee_Logo.png" className='ImgRollingCoffe'></img></Navbar.Brand>
+            <Navbar.Brand as={Link} to='/'><img src="/assets/Coffee_Logo.png" className='ImgRollingCoffe'></img></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" >
               <Nav className="ms-auto">
-                <Nav.Link href="#home" active>Inicio</Nav.Link>
-                <Nav.Link href="#link">Administrador</Nav.Link>
-                <Nav.Link href="#link">Registro</Nav.Link>
-                <Nav.Link href="#link">Login</Nav.Link>
+                <NavLink end className='nav-link' to='/'>Inicio</NavLink>
+                <NavLink end className='nav-link' to='/administrador'>Administrador</NavLink>
+                <NavLink end className='nav-link' to='/login'>Registro</NavLink>
+                <NavLink end className='nav-link' to='/registro'>Login</NavLink>
                 
               </Nav>
             </Navbar.Collapse>
