aad5d39145bbabee765d017c51153267156a6115
Natalia Lopez <natigab.24@gmail.com>
2024-02-22 15:56:47 -0300

    agregamos la funcion borrarProducto en el componente ItemProducto

--- src/components/ItemProducto.jsx
+++ src/components/ItemProducto.jsx
@@ -1,6 +1,29 @@
 import { Button, Image } from "react-bootstrap";
+import Swal from "sweetalert2";
+import { borrarProductoAPI } from "../helpers/queries";
+const ItemProducto = ({ producto }) => {
+  const borrarProducto = () => {
+    Swal.fire({
+      title: "¿Estás seguro de eliminar el producto?",
+      text: "No puedes revertir este proceso posteriormente!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        borrarProductoAPI(producto.id)
 
-const ItemProducto = ({producto}) => {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
   return (
     <tr>
       <td>{producto.id}</td>
@@ -14,7 +37,7 @@ const ItemProducto = ({producto}) => {
         <Button variant="warning" className="me-lg-2">
           <i className="bi bi-pencil-square"></i>
         </Button>
-        <Button variant="danger" className="me-lg-2">
+        <Button variant="danger" className="me-lg-2" onClick={borrarProducto}>
           <i className="bi bi-trash"></i>
         </Button>
       </td>
