80c792e617d2e74f16c90e0a6bc2de3b3e681401
Natalia Lopez <natigab.24@gmail.com>
2024-02-22 20:53:47 -0300

    detalle de producto terminado

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -1,56 +1,60 @@
-import { Container, Row, Image } from "react-bootstrap";
-import "../../App.css";
-import{useParams} from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { obtenerProductoAPI } from "../../helpers/queries";
 import Swal from "sweetalert2";
+
+
 const DetalleProducto = () => {
-  const {id} = useParams();
-  const [producto, setProducto] = useState({});
-  useEffect(()=>{
-    //buscar el producto que quiero maquetar
-    cargarDetalle();
-  },[])
-  const cargarDetalle = async()=>{
-    const respuesta = await obtenerProductoAPI(id)
-    if (respuesta.status === 200) {
-      //mostrar el producto en la card
-      const datoProducto = await respuesta.jason();
-      setProducto(datoProducto);
-    }else{
-      Swal.fire({
-        title: "Ocurrio un error",
-        text: `Intente crear este producto en unos minutos`,
-        icon: "error"
-      });
-    }
+const {id} = useParams();
+const [producto, setProducto] = useState({});
+
+useEffect(()=>{
+  //buscar el producto que quiero maquetar
+  cargarDetalle();
+},[])
+
+const cargarDetalle = async()=>{
+  const respuesta = await obtenerProductoAPI(id)
+  if(respuesta.status === 200){
+    //mostrar el producto en la card
+    const datoProducto = await respuesta.json();
+    setProducto(datoProducto);
+  }else{
+    Swal.fire({
+      title: "Ocurrio un error",
+      text: "Intente realizar esta operacion en unos minutos",
+      icon: "error",
+    });
   }
+}
+
   return (
-    <Container>
-      <Row className="border border-1 rounded m-5 p-3">
-        <div className="col-md-6">
-          <Image
-            src="../../../assets/cafe-americano.jpg"
-            className="figure-img img-fluid rounded"
-            fluid
-            width={500}
-          />
-        </div>
-        <div className="col-md-6">
-          <h4>Capuccino</h4>
-          <hr></hr>
-          <p>
-            El café americano es una bebida caliente que consiste en un espresso
-            diluido con agua caliente, lo que resulta en una taza de café suave
-            y aromatico. Es una opción popular para aquellos que prefieren un
-            café menos intenso que el espresso tradicional. Perfecto para
-            disfrutar en cualquier momento del día.
-          </p>
-          <p>
-            <span className="textGreen fw-bold">Categoría:</span> infusiones
-          </p>
-          <p className="textGreen fw-bold">Precio: $1000</p>
-        </div>
-      </Row>
+    <Container className="my-3 mainSection">
+      <Card>
+        <Row>
+          <Col md={6}>
+            <Card.Img
+              variant="top"
+              src={producto.imagen}
+              alt={producto.nombreProducto}
+            />
+          </Col>
+          <Col md={6}>
+            <Card.Body>
+              <Card.Title className="primary-font">{producto.nombreProducto}</Card.Title>
+              <hr />
+              <Card.Text>
+              {producto.descripcion_amplia}
+              <br/>
+              <br/>
+              <span className="primary-font fw-semibold ">Categoria:</span> {producto.categoria}
+              <br className='mb-3'/>
+              <span className="primary-font fw-semibold ">Precio: ${producto.precio}</span></Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
     </Container>
   );
 };
